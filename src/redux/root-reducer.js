33957c39b2ba7e741ed92da352393ce6a080e5a3
Khubi Shah <shah.khubi@gmail.com>
2020-12-31 15:43:05 -0500

    moved our directory and shop data into its own reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,24 @@
 import { combineReducers } from 'redux';
-
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer.jsx';
 import cartReducer from './cart/cart.reducer.jsx';
-export default combineReducers({
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] // tells redux that the only reducer that we want to persist is the cartReducer
+}; 
+
+
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
